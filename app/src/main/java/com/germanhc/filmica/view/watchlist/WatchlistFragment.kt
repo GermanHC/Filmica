4925b4015616aa6924d82f38c32a3137d083c4d1
GermanHC <ghcaunat@gmail.com>
2018-12-09 23:55:41 +0100

    Snackbar

--- app/src/main/java/com/germanhc/filmica/view/watchlist/WatchlistFragment.kt
+++ app/src/main/java/com/germanhc/filmica/view/watchlist/WatchlistFragment.kt
@@ -3,6 +3,7 @@ package com.germanhc.filmica.view.watchlist
 
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
@@ -10,11 +11,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_watchlist.*
-
 import com.germanhc.filmica.R
 import com.germanhc.filmica.data.Film
 import com.germanhc.filmica.data.FilmsRepo
 import com.germanhc.filmica.view.util.SwipeToDeleteCallback
+
 
 class WatchlistFragment : Fragment() {
     lateinit var listener: OnItemClickListener
@@ -62,7 +63,10 @@ class WatchlistFragment : Fragment() {
     private fun setupSwipeHandler() {
         val swipeHandler = object : SwipeToDeleteCallback() {
             override fun onSwiped(holder: RecyclerView.ViewHolder, direction: Int) {
-                deleteFilmAt(holder.adapterPosition)
+                val film = adapter.getFilm(holder.adapterPosition)
+                deleteFilmAt(holder.adapterPosition, film)
+                Snackbar.make(holder.itemView, getString(R.string.snackbarDeleteElement), Snackbar.LENGTH_LONG)
+                    .setAction(R.string.undo) { restoreFilm(film) }.show()
             }
         }
 
@@ -70,10 +74,16 @@ class WatchlistFragment : Fragment() {
         itemTouchHelper.attachToRecyclerView(watchlist)
     }
 
-    private fun deleteFilmAt(position: Int) {
-        val film = adapter.getFilm(position)
+
+    private fun deleteFilmAt(position: Int, film: Film) {
         FilmsRepo.deleteFilm(context!!, film) {
             adapter.removeFilmAt(position)
+        }
+    }
+
+    private fun restoreFilm(film: Film) {
+        FilmsRepo.saveFilm(context!!, film) {
+            loadWatchlist()
         }
     }
 
